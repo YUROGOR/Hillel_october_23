6ed3eb1e7e3b7685b51de03ebe96773e83e7ebdc
Юрий <yrogor@ukr.net>
2023-12-13 23:41:59 +0200

    add hw_lesson_11

---
+++ lesson_11/test_lesson_11.py
@@ -0,0 +1,21 @@
+import time
+
+
+def test_sleep_1():
+    time.sleep(2)
+
+
+def test_sleep_2():
+    time.sleep(2)
+
+
+def test_sleep_3():
+    time.sleep(2)
+
+
+def test_sleep_4():
+    time.sleep(2)
+
+
+def test_unique():
+    time.sleep(2)
